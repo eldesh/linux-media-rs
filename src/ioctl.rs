4cd8c174f8a6f6dbedf4893e8e2d2d9f4db52487
eldesh <nephits@gmail.com>
2025-03-14 23:52:02 +0900

    Fix: avoid 'return' which jump out from expanded context

--- src/ioctl.rs
+++ src/ioctl.rs
@@ -3,11 +3,12 @@ macro_rules! ioctl {
     ($fd:expr, $kind:expr, $arg:expr) => {{
         let ret = libc::ioctl($fd.as_raw_fd(), $kind, $arg);
         if ret != 0 {
-            return Err(error::Error::Ioctl {
+            Err(error::Error::Ioctl {
                 code: ret,
                 api: $kind,
-            });
+            })
+        } else {
+            Ok(())
         }
-        Ok(())
     }};
 }
