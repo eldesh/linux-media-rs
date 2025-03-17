0bcb7250f8f1c350a53802a0d4a6e3ebe17c43a8
eldesh <nephits@gmail.com>
2025-03-17 18:15:32 +0900

    Add: MediaPadDesc into raw type

--- src/media_pad_desc.rs
+++ src/media_pad_desc.rs
@@ -6,21 +6,31 @@ use crate::MediaPadFlags;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Serialize, Deserialize)]
 pub struct MediaPadDesc {
-    /// ID of the entity this pad belongs to.
     entity: EntityId,
-    /// Pad index
     index: usize,
-    /// Pad flags
     flags: MediaPadFlags,
 }
 
 impl MediaPadDesc {
+    pub fn new(entity: EntityId, index: usize, flags: MediaPadFlags) -> Self {
+        Self {
+            entity,
+            index,
+            flags,
+        }
+    }
+
+    /// ID of the entity this pad belongs to.
     pub fn id(&self) -> EntityId {
         self.entity
     }
+
+    /// Pad index
     pub fn index(&self) -> usize {
         self.index
     }
+
+    /// Pad flags
     pub fn flags(&self) -> MediaPadFlags {
         self.flags
     }
@@ -33,5 +43,15 @@ impl From<media::media_pad_desc> for MediaPadDesc {
             index: desc.index.into(),
             flags: desc.flags.try_into().unwrap(),
         }
+    }
+}
+
+impl From<MediaPadDesc> for media::media_pad_desc {
+    fn from(desc: MediaPadDesc) -> media::media_pad_desc {
+        let mut raw: media::media_pad_desc = unsafe { std::mem::zeroed() };
+        raw.entity = desc.entity.into();
+        raw.index = desc.index as u16;
+        raw.flags = desc.flags.into();
+        raw
     }
 }
