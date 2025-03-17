8c7f92860c67b9b5eff5c4548885470b6680aa8f
eldesh <nephits@gmail.com>
2025-03-17 18:17:28 +0900

    Add: qualify names in ioctl!

--- src/ioctl.rs
+++ src/ioctl.rs
@@ -3,7 +3,7 @@ macro_rules! ioctl {
     ($fd:expr, $kind:expr, $arg:expr) => {{
         let ret = libc::ioctl($fd.as_raw_fd(), $kind, $arg);
         if ret != 0 {
-            Err(error::Error::Ioctl {
+            std::result::Result::Err(crate::error::Error::Ioctl {
                 code: std::io::Error::from_raw_os_error(ret),
                 api: $kind,
             })
