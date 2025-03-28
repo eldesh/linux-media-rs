a33bd4724e951cdd7c222a742651e7815d8bdaed
eldesh <nephits@gmail.com>
2025-03-29 03:23:33 +0900

    Add: an example enumerates media devices
    The example enumerates media devices with the specific driver.

---
+++ examples/pi5videos/main.rs
@@ -0,0 +1,94 @@
+use std::fs;
+use std::io;
+use std::path::{Path, PathBuf};
+
+use linux_media as media;
+use regex::Regex;
+
+use media::error::trap_io_error;
+
+struct MediaDeviceIterator {
+    path: PathBuf,
+    /// A pattern matches driver name of enumerated media device interfaces.
+    driver: Regex,
+    /// iterator traverses the path and generates file paths of device files.
+    iter: Box<dyn Iterator<Item = PathBuf> + 'static>,
+}
+
+/// Enumerate paths of media devices that file name contains the specified driver name.
+impl Iterator for MediaDeviceIterator {
+    type Item = PathBuf;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            Some(item) if self.driver.is_match(&item.to_string_lossy()) => {
+                item.file_name().map(|file| Path::new("/dev").join(file))
+            }
+            Some(_) | None => None,
+        }
+    }
+}
+
+impl MediaDeviceIterator {
+    pub fn new(driver: Regex) -> io::Result<Self> {
+        let sysfs = Path::new("/sys/bus/media/devices");
+        Self::with_sysfs(sysfs, driver)
+    }
+
+    pub fn with_sysfs<P>(sysfs: P, driver: Regex) -> io::Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let iter = Box::new(
+            sysfs
+                .as_ref()
+                .read_dir()?
+                .filter_map(|e| e.ok())
+                .filter(|dev| dev.path().is_symlink())
+                .filter_map(|dev| fs::read_link(&dev.path()).ok()),
+        );
+
+        Ok(Self {
+            path: sysfs.as_ref().to_path_buf(),
+            driver,
+            iter,
+        })
+    }
+}
+
+fn media_devices(driver: Regex) -> io::Result<MediaDeviceIterator> {
+    MediaDeviceIterator::new(driver)
+}
+
+fn read_link<P: AsRef<Path>>(path: P) -> media::error::Result<PathBuf> {
+    let path = path.as_ref();
+    Ok(fs::read_link(path).map_err(|e| trap_io_error(e, path.to_path_buf()))?)
+}
+
+fn read_to_string<P: AsRef<Path>>(path: P) -> media::error::Result<String> {
+    let path = path.as_ref();
+    Ok(fs::read_to_string(path).map_err(|e| trap_io_error(e, path.to_path_buf()))?)
+}
+
+fn main() -> media::error::Result<()> {
+    for media_node in media_devices(Regex::new("pisp_be").unwrap()).unwrap() {
+        dbg!(media_node.display());
+        let media = media::Media::from_path(&media_node).unwrap();
+        let topology = media::MediaTopologyBuilder::new()
+            .get_interface()
+            .from_fd(media.info(), media.device_fd())?;
+
+        for char_dev in topology
+            .interfaces_slice()
+            .iter()
+            .filter(|intf| intf.r#type() == media::MediaInterfaceType::V4LVideo)
+            .map(|intf| intf.devnode().into())
+            .filter(|char_dev: &PathBuf| char_dev.is_symlink())
+        {
+            let dev_name = read_to_string(&dbg!(char_dev.join("name")))?;
+            let char_dev_link = read_link(&char_dev)?;
+            let dev_node = Path::new("/dev").join(dbg!(&char_dev_link).file_name().unwrap());
+            println!("{}\t({})", dev_name, dev_node.display());
+        }
+    }
+    Ok(())
+}
